a435ad3204d2baa2177f6564eb027d640d0be585
Kai <100342931+DeeJax71@users.noreply.github.com>
2023-02-20 21:50:29 +1300

    Roughly Finished game

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    private float timer = 0.0f;
-    [SerializeField] TextMeshProUGUI timerText, endScreenText;
+    private float timer = 0.0f, finalScore;
+    [SerializeField] TextMeshProUGUI timerText, endScreenScore, endScreenWinLoss;
 
+    [ContextMenu("Score")]
     // Start is called before the first frame update
     void Start()
     {
@@ -18,27 +19,40 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float t  = Time.time - timer;
+        float t = Time.time - timer;
 
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f0");
+        string minutes = ((int)t / 59).ToString();
+        string seconds = (t % 59).ToString("f0");
 
         timerText.text = minutes + ":" + seconds;
 
         if (Time.timeScale == 0f)
         {
-            int scoreMinutes = Convert.ToInt32(minutes) * 60;
+            int scoreMinutes = Convert.ToInt32(minutes);
             int scoreSeconds = Convert.ToInt32(seconds);
-            float finalScore = ((float)scoreMinutes * (float)scoreSeconds) / 30* 500;
 
-            Math.Round((Decimal)finalScore, 3, MidpointRounding.AwayFromZero);
+            if (Convert.ToInt32(minutes) >= 1 && Convert.ToInt32(seconds) >= 0)
+            {
+                finalScore = (((float)scoreMinutes * 60 - scoreSeconds) * (float)scoreSeconds + 1) / 30 * 100;
 
-            Debug.Log(minutes);
-                Debug.Log(seconds);
-            Debug.Log(scoreMinutes);
-            Debug.Log(scoreSeconds);
-            Debug.Log(finalScore);
-            endScreenText.text = finalScore.ToString() + " points!";
+                Math.Round((Decimal)finalScore, 0, MidpointRounding.AwayFromZero);
+            }
+            else
+            {
+                finalScore = ((float)scoreSeconds) / 30 * 600;
+
+                Math.Round((Decimal)finalScore, 0, MidpointRounding.AwayFromZero);
+            }
+            endScreenScore.text = finalScore.ToString("F0") + " points!";
+
+            if (Convert.ToInt32(minutes) == 0 && Convert.ToInt32(seconds) <= 55)
+            {
+                endScreenWinLoss.text = "You Win!";
+            }
+            else
+            {
+                endScreenWinLoss.text = "You Lose!";
+            }     
         }
-    }
+    }   
 }
